4bfa73d19e6cdf48f0a1f16a067e52ca424ccf54
Tharun_Gudla <tharunteja.gudla@gmail.com>
2025-03-27 17:07:22 -0500

    Added logic to display invalid address

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import Heading from './Heading/Heading'
 import CalculatorBox from './CalculatorBox/CalculatorBox'
 import Error from './Error/Error'
 import { ErrorContext } from './store/context'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HistoryPage from './History/HistoryPage'
 
 function App() {
- 
+
   const [display, setDisplay] = useState(false);
+  
 
   return (
     <>
 
-      <ErrorContext.Provider value={{display, setDisplay}}>
-      <Heading />
-      <CalculatorBox />
-      <div>
-        <Error />
-      </div>
+      <ErrorContext.Provider value={{ display, setDisplay }}>
+        <BrowserRouter>
+          <Heading />
+          <Routes>
+            <Route path='/' element={<CalculatorBox />}></Route>
+            <Route path='/history' element={<HistoryPage />}></Route>
+          </Routes>
+          <div>
+            <Error />
+          </div>
+        </BrowserRouter>
       </ErrorContext.Provider>
-      
-      
+
+
+
     </>
   )
 }
